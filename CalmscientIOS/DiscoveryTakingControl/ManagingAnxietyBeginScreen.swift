25e6af900bd73a16b7ae99a5c42d3d351cb99e10
BVK <srikanthbvk2020@gmail.com>
2024-08-22 00:04:14 +0530

    Taking Control issues fixed

---
+++ CalmscientIOS/DiscoveryTakingControl/ManagingAnxietyBeginScreen.swift
@@ -0,0 +1,29 @@
+//
+//  ManagingAnxietyBeginScreen.swift
+//  CalmscientIOS
+//
+//  Created by BVK on 21/08/24.
+//
+
+import UIKit
+
+class ManagingAnxietyBeginScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
